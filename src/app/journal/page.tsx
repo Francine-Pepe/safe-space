508ea3d36681fb6c85a4205cb616f6311f78e671
Francine-Pepe <franmelopepe@gmail.com>
2026-07-08 16:26:07 +0200

    journal page

--- src/app/journal/page.tsx
+++ src/app/journal/page.tsx
@@ -1,7 +1,15 @@
+import Notebook from "@/components/Notebook/Notebook";
 import React from "react";
+import styles from "./page.module.scss";
 
-function page() {
-  return <div>page</div>;
+function Page() {
+  return (
+    <main className={styles.container}>
+      <h1> Empty your mind</h1>
+      <p>You do not have to make sense. Nobody will judge this. Just write.</p>
+      <Notebook />
+    </main>
+  );
 }
 
-export default page;
+export default Page;
