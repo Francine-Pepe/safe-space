0fbb0f43d61beb9f6990cedd96b93ea3748abe52
Francine-Pepe <franmelopepe@gmail.com>
2026-07-09 14:42:34 +0200

    feat: notebook bg added to the page

--- src/app/journal/page.tsx
+++ src/app/journal/page.tsx
@@ -1,11 +1,14 @@
 import Notebook from "@/components/Notebook/Notebook";
 import React from "react";
 import styles from "./page.module.scss";
+import Background from "@/components/Background/Background";
 
 function Page() {
   return (
     <main className={styles.container}>
-      <h1> Empty your mind</h1>
+      <Background variant="notebook" /> 
+      <h1> Let it out.</h1>
+      <h2>You do not have to carry everything</h2>
       <p>You do not have to make sense. Nobody will judge this. Just write.</p>
       <Notebook />
     </main>
